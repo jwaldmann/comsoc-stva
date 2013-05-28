efe981282820c7c1fe48c6703a7a6d8a772b08e2
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-05-28 21:35:28 +0200

    proper handling of candidates missing from votes

--- Main.hs
+++ Main.hs
@@ -12,7 +12,7 @@ main = do
     case runP votes () "/dev/stdin" input of
         Left err -> print err
         Right vs -> print $ stv vs
-        
+
 type Candidate = [String]
 
 votes :: Parser (Votes Candidate)
